1b22ffbd1c0e15aa158721994a153b74f52859d7
Onkabetse <113599354+Onkabetse077@users.noreply.github.com>
2024-01-09 12:55:01 +0200

    A Role Playing Game

--- js/script.js
+++ js/script.js
@@ -20,37 +20,52 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+//Objects
+const locations = [{
+  name:"town square",
+  "button text":["Go to store", "Go to cave","Fight dragon"],
+  "button functions":[goStore, goCave,fightDragon],
+  text: "You are in the town square. You see a sign that says \"Store\"."
+},{
+  name:"store",
+  "button text":["Buy 10 health (10 gold)", "Buy weapon (30 gold)","Go to town square"],
+  "button functions":[buyHealth,buyWeapon,goTown],
+  text: "You enter the store."
+},{
+   name:"cave",
+   "button text":["Fight slime","Fight fanged beast","Go to town square"],
+  "button functions":[fightSlime,fightBeast,goTown],
+  text:"You enter the cave. You see some monsters."
+}];
+
+
 //Initialize Buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 //Button Functions
+function update(location){
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+
+  text.innerText = location.text;
+}
+
 function goTown(){
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight Dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says Store.";
+  update(locations[0]);
 }
 function goStore(){
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-
-  //Updates Health and Weapon stats
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-
-  //Updating Text
-  text.innerText ="You enter the store.";
+  update(locations[1]);
 }
 
 function goCave(){
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 
@@ -62,6 +77,6 @@ function buyHealth(){}
 
 function buyWeapon(){}
 
+function fightSlime(){}
 
-
-
+function fightBeast(){}
