a425d7b6be4ab713a64c187b6ac49d5dc44a56c0
Onkabetse <113599354+Onkabetse077@users.noreply.github.com>
2024-01-10 15:14:32 +0200

    A Role Playing Game

--- js/script.js
+++ js/script.js
@@ -58,7 +58,13 @@ const locations = [{
   "button text":["Attack","Dodge","Run"],
   "button functions":[attack,dodge,goTown],
   text:"You are fighting a monster."
-}];
+},
+  {
+    name: "kill monster",
+    "button text": ["Go to town square", "Go to town square","Go to town square"],
+    "button functions": [goTown, goTown, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  }];
 
 const battle = [];
 
@@ -69,6 +75,8 @@ button3.onclick = fightDragon;
 
 //Button Functions
 function update(location){
+  monsterStats.style.display ="none";
+
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
@@ -167,8 +175,31 @@ function attack(){
 
   text.innerText = " You attack it with your " + weapons[currentWeapon].name + ".";
   health -=monsters[fighting].level;
-  monsterHealth -= weapons[currentWeapon].power;
+  monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random()* xp) + 1;
+
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth
+  if(health >= 0){
+    lose();
+  }else if(monsterHealth <= 0){
+    defeatMonster();
+  }
 }
 
 function dodge(){
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
+
+function defeatMonster(){
+  gold +=Math.floor(monsters[fighting].level* 6.7);
+  xp += monsters[fighting].level;
+
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
+function lose(){
+  update(locations[4]);
+}
+
